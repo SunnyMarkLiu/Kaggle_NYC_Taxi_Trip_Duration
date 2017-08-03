ca09227c68b9e31c31d310cb41f7e752533463a8
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-08-03 20:12:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Kaggle_NYC_Taxi_Trip_Duration
+Kaggle Competition: [New York City Taxi Trip Duration](https://www.kaggle.com/c/nyc-taxi-trip-duration)
+
+## Contents
+
+## License
+This project is licensed under the terms of the MIT license.
---
+++ conf/configure.py
@@ -0,0 +1,19 @@
+#!/usr/local/miniconda2/bin/python
+# _*_ coding: utf-8 _*_
+
+"""
+@author: MarkLiu
+@time  : 17-6-26 下午3:14
+"""
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('..'))
+sys.path.append(module_path)
+
+import time
+
+
+class Configure(object):
+
+    submission_path = '../result/submission_{}.csv'.format(time.strftime('%Y_%m_%d_%H_%M_%S', time.localtime(time.time())))
