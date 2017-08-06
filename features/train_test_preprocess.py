1e4e77c47f80f40fc3eb254b3e9b177017d5e7dc
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-08-06 15:47:21 +0800

    generate date features

--- features/train_test_preprocess.py
+++ features/train_test_preprocess.py
@@ -11,11 +11,36 @@ import sys
 module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
 
+import pandas as pd
 from utils import data_utils
+
+
+def generate_date_features(train, test):
+    # 2016-03-14 17:24:55
+    train['pickup_datetime'] = pd.to_datetime(train['pickup_datetime'])
+    train['dropoff_datetime'] = pd.to_datetime(train['dropoff_datetime'])
+    test['pickup_datetime'] = pd.to_datetime(test['pickup_datetime'])
+    # date
+    train.loc[:, 'pickup_date'] = train['pickup_datetime'].dt.date
+    test.loc[:, 'pickup_date'] = test['pickup_datetime'].dt.date
+    # month
+    train['pickup_month'] = train['pickup_datetime'].dt.month
+    test['pickup_month'] = train['pickup_datetime'].dt.month
+    # day
+    train['pickup_day'] = train['pickup_datetime'].dt.day
+    test['pickup_day'] = train['pickup_datetime'].dt.day
+    # hour
+    train['pickup_hour'] = train['pickup_datetime'].dt.hour
+    test['pickup_hour'] = train['pickup_datetime'].dt.hour
+
 
 def main():
     train, test = data_utils.load_dataset()
     print 'train: {}, test: {}'.format(train.shape, test.shape)
+    print 'generate date features...'
+    generate_date_features(train, test)
+    print 'train: {}, test: {}'.format(train.shape, test.shape)
+
 
 if __name__ == '__main__':
     print '========== perform simple train test preprocess =========='
