59490c7132910f89011749b5be4708568f6c6b1d
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-08-10 14:09:39 +0800

    optimize dtypes to save memory

--- features/data_cleaning.py
+++ features/data_cleaning.py
@@ -33,6 +33,22 @@ def main():
     # train = train[train['trip_duration'] < 500000] # 导致过拟合
 
     print 'train: {}, test: {}'.format(train.shape, test.shape)
+
+    # optimize dtypes
+    print('Memory usage, Mb: {:.2f}'.format(train.memory_usage().sum() / 2 ** 20))
+    print 'optimize dtypes...'
+    train['is_store_and_fwd_flag'] = train['is_store_and_fwd_flag'].astype(np.uint8)
+    train['passenger_count'] = train['passenger_count'].astype(np.uint8)
+    train['vendor_id'] = train['vendor_id'].astype(np.uint8)
+    train['pickup_month'] = train['pickup_month'].astype(np.uint8)
+    train['pickup_day'] = train['pickup_day'].astype(np.uint8)
+    train['pickup_hour'] = train['pickup_hour'].astype(np.uint8)
+    train['pickup_weekofyear'] = train['pickup_weekofyear'].astype(np.uint8)
+    train['pickup_weekday'] = train['pickup_weekday'].astype(np.uint8)
+    train['is_weekend'] = train['is_weekend'].astype(np.uint8)
+    train['trip_duration'] = train['trip_duration'].astype(np.uint32)
+    print('After optimized memory usage, Mb: {:.2f}'.format(train.memory_usage().sum() / 2 ** 20))
+
     print 'save dataset...'
     data_utils.save_dataset(train, test, op_scope='2')
 
