bd95bda779a9e61f7edc386260d02e0e64223eab
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-09-09 13:20:29 +0800

    clean code

--- model/xgboost_model.py
+++ model/xgboost_model.py
@@ -13,14 +13,12 @@ sys.path.append(module_path)
 
 import numpy as np
 import pandas as pd
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
 import xgboost as xgb
 from xgboost import callback
 
 # my own module
 from conf.configure import Configure
-from utils import data_utils, feature_util
+from utils import data_utils
 
 
 def main():
@@ -55,16 +53,12 @@ def main():
     print 'feature check before modeling...'
     # feature_util.feature_check_before_modeling(train, test, train.columns)
 
-    train_rmses = []
-    val_rmses = []
-    num_boost_roundses = []
-
     X_test = test
     df_columns = train.columns.values
     dtest = xgb.DMatrix(X_test, feature_names=df_columns)
 
     xgb_params = {
-        'eta': 0.005,
+        # 'eta': 0.005,
 
         'min_child_weight': 1,
         'reg_lambda': 0.006,
@@ -82,45 +76,27 @@ def main():
         'silent': 1
     }
 
-    for i in range(0, 1):
-        random_state = 42 + i
-        X_train, X_val, y_train, y_val = train_test_split(train, y_train_all, test_size=0.25, random_state=random_state)
-        print 'X_train:', X_train.shape, ', X_val:', X_val.shape
+    learning_rates = [0.01] * 800 + [0.007] * 1000 + [0.005] * 1000 + [0.003] * 1000 + [0.001] * 1200
 
-        dtrain = xgb.DMatrix(X_train, y_train, feature_names=df_columns)
-        dval = xgb.DMatrix(X_val, y_val, feature_names=df_columns)
+    dtrain = xgb.DMatrix(train, y_train_all, feature_names=df_columns)
 
-        cv_result = xgb.cv(dict(xgb_params),
-                           dtrain,
-                           num_boost_round=500,
-                           early_stopping_rounds=50,
-                           verbose_eval=50,
-                           show_stdv=False,
-                           callbacks=callback.reset_learning_rate([0.01] * 500 + [0.007] * 500 + [0.005] * 2000 +
-                                                                  [0.003] * 2000 + [0.001] * 1000)
-                           )
+    cv_result = xgb.cv(dict(xgb_params),
+                       dtrain,
+                       num_boost_round=5000,
+                       early_stopping_rounds=50,
+                       verbose_eval=50,
+                       show_stdv=False,
+                       callbacks=[callback.reset_learning_rate(learning_rates)]
+                       )
 
-        num_boost_rounds = len(cv_result)
-        num_boost_roundses.append(num_boost_rounds)
-        model = xgb.train(dict(xgb_params), dtrain, num_boost_round=num_boost_rounds)
-        train_rmse = mean_squared_error(dtrain.get_label(), model.predict(dtrain))
-        val_rmse = mean_squared_error(dval.get_label(), model.predict(dval))
-        print 'perform {} cross-validate: train rmse = {}, validate rmse = {}'.format(i + 1, train_rmse,
-                                                                                      val_rmse)
-        train_rmses.append(train_rmse)
-        val_rmses.append(val_rmse)
-
-    print '\naverage train rmse = {} average validate rmse = {}'.format(
-        sum(train_rmses) / len(train_rmses),
-        sum(val_rmses) / len(val_rmses))
-
-    best_num_boost_rounds = sum(num_boost_roundses) // len(num_boost_roundses)
+    best_num_boost_rounds = len(cv_result)
     print 'best_num_boost_rounds =', best_num_boost_rounds
     # train model
     print 'training on total training data...'
-    dtrain_all = xgb.DMatrix(train, y_train_all, feature_names=df_columns)
-    model = xgb.train(dict(xgb_params, base_score=np.mean(y_train_all)), dtrain_all,
-                      num_boost_round=best_num_boost_rounds)
+    train_learning_rates = learning_rates[:best_num_boost_rounds]
+    model = xgb.train(dict(xgb_params, base_score=np.mean(y_train_all)), dtrain,
+                      num_boost_round=best_num_boost_rounds,
+                      callbacks=[callback.reset_learning_rate(train_learning_rates)])
 
     print 'predict submit...'
     y_pred = model.predict(dtest)
